d7b7a29cab4b827b01ebce396b5e7eb4b7d28f69
PetterChanHyuk <pchishahaboy@naver.com>
2026-03-21 09:11:58 +0900

    버그 수정

--- init-db/source_db.sql
+++ init-db/source_db.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS card_ledger;
 
 CREATE TABLE card_ledger (
                              ledger_id       BIGINT AUTO_INCREMENT PRIMARY KEY,
-                             rrn             VARCHAR(12) UNIQUE NOT NULL,
+                             rrn             VARCHAR(12) UNIQUE NOT NULL,  -- 카드사 발급
+                             stan            VARCHAR(6)  UNIQUE NOT NULL,  -- VAN 발급
                              card_number     VARCHAR(19) NOT NULL,
                              merchant_id     VARCHAR(15) NOT NULL,
                              amount          BIGINT NOT NULL,
